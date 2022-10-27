8dab8476e02ddcf8dbacf45677e29897418e0151
hiradsajde <hiradsojde@gmail.com>
2022-10-28 00:09:21 +0330

    Inital commit

---
+++ ip-forward.sh
@@ -0,0 +1,6 @@
+read -p 'enter main server ip address : ' ipv4
+read -p 'enter main server port number : ' port
+sudo iptables -t nat -A PREROUTING -p udp --dport $port -j DNAT --to-destination $ipv4
+sudo iptables -t nat -A PREROUTING -p tcp --dport  $port   -j DNAT --to-destination $ipv4
+sudo iptables -t nat -A POSTROUTING -p udp -d $ipv4 --dport  $port  -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -p tcp -d $ipv4 --dport  $port  -j MASQUERADE
